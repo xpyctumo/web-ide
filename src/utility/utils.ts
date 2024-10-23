716ccb163b7eda0046f5fede9563291be3db4f6f
Rahul Yadav <rahulkyadav57@gmail.com>
2024-10-23 12:46:00 +0530

    fix: correct Base64 decoding to support UTF-8 characters (#132)

--- src/utility/utils.ts
+++ src/utility/utils.ts
@@ -43,7 +43,7 @@ export const encodeBase64 = (data: string) => {
   return Buffer.from(data).toString('base64');
 };
 export const decodeBase64 = (data: string) => {
-  return Buffer.from(data, 'base64').toString('ascii');
+  return Buffer.from(data, 'base64').toString('utf-8');
 };
 
 export const objectToJSON = (obj: object) => {
